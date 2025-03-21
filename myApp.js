8c2167526a717ae37ee252f2c3bd5cd1db99a82f
Joshua <joshuahawi24@gmail.com>
2025-03-21 11:52:30 +0300

    Am I done with to upper?

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 if (process.env.MESSAGE_STYLE === "uppercase") {
-  message = message.toUpperCase();
+  message = "HELLO JSON";
   console.log("I am not insane")
 } else {
   console.log("Why isn't it working")
